4f7882f0473efd17d6106572247f3b362e3fe5eb
Micah Banschick <micha.ban@gmail.com>
2021-04-21 18:17:05 +0000

    Automatically backed up by Learn

--- src/Cell.js
+++ src/Cell.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class Matrix extends Component {
+export default class Cell extends Component {
     constructor(props) {
         super()
         this.state = {color: props.value}
